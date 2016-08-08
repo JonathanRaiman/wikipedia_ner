b6215c38a58e019a566411445ff440c2a2ea9d2b
Jonathan Raiman <jonathanraiman@gmail.com>
2016-08-08 07:06:33 +0200

    accelerate redirection resolution

--- resolve_redirections.py
+++ resolve_redirections.py
@@ -9,6 +9,7 @@ def main(path, outpath):
     redirects = 0
     t0 = time.time()
     speed = 0.0
+    increment = 100
     for page_title, destination in get_redirection_list(wiki):
         # can we skip a step?
         redirects += 1
@@ -19,8 +20,8 @@ def main(path, outpath):
         else:
             # if not wire up page_title to destination
             redirects_to[page_title] = destination
-        if redirects % 100 == 0:
-            speed = 0.8 * speed + 0.2 * ((1000.0) / (time.time() - t0))
+        if redirects % increment == 0:
+            speed = 0.8 * speed + 0.2 * (float(increment) / (time.time() - t0))
             t0 = time.time()
             print("%d so far, %.3f redirects/s     \r" % (redirects, speed), end="", flush=True)
 
@@ -28,25 +29,46 @@ def main(path, outpath):
     total_rewires = 0
     rewires = 0
     epoch = 0
+    changed = None
     while True:
+        new_changed = set()
         rewires = 0
+        to_remove = set()
         print("Epoch %d, total_rewires %d" % (epoch, total_rewires))
-        for link, value in redirects_to.items():
-            if value in redirects_to:
-                redirects_to[link] = redirects_to[value]
-                rewires += 1
+        if epoch == 0:
+            for link, value in redirects_to.items():
+                if value in redirects_to:
+                    redirects_to[link] = redirects_to[value]
+                    new_changed.add(link)
+                    rewires += 1
+        else:
+            for link in changed:
+                value = redirects_to[link]
+                if value in redirects_to:
+                    if value == link:
+                        to_remove.add(value)
+                    else:
+                        redirects_to[link] = redirects_to[value]
+                        new_changed.add(link)
+                        rewires += 1
+
+        for link in to_remove:
+            del redirects_to[link]
 
         total_rewires += rewires
         if rewires is 0:
             break
+        changed = new_changed
+        epoch += 1
 
-    with gzip.open(outpath, "wt") as f:
+    with open(outpath, "wt") as f:
         for source, target in redirects_to.items():
-            f.write("%s->%s\n" % (source, target))
+            f.write("%s;%s\n" % (source, target))
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("resolve_redirections [wiki_dump_path.bz2] [outpath]")
+        sys.exit(1)
     dump_path = sys.argv[1]
     outpath = sys.argv[2]
     main(dump_path, outpath)
